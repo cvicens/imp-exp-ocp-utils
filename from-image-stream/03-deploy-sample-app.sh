33f9b4c0e856e7c34c5fb7c2243e933593755b20
Carlos Vicens <carlos.vicens.alonso@gmail.com>
2018-12-05 12:09:48 +0100

    new and error fixed in 03-deploy...

--- from-image-stream/03-deploy-sample-app.sh
+++ from-image-stream/03-deploy-sample-app.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
-if [ "$#" -lt 3 ]; then
-  echo "Usage: $0 FROM_IMAGE_STREAM_FILE NAMESPACE GIT_URL GIT_CONTEXT_DIR" >&2
+if [ "$#" -lt 4 ]; then
+  echo "Usage: $0 FROM_IMAGE_STREAM_FILE LOCAL_IMAGE_STREAM_NAMESPACE NAMESPACE GIT_URL [GIT_CONTEXT_DIR]" >&2
   exit 1
 fi
 
 FROM_IMAGE_STREAM_FILE=$1
-NAMESPACE=$2
-GIT_URL=$3
-GIT_CONTEXT_DIR=$4
+LOCAL_IMAGE_STREAM_NAMESPACE=$2
+NAMESPACE=$3
+GIT_URL=$4
+GIT_CONTEXT_DIR=$5
 
 if [ ! -z ${GIT_CONTEXT_DIR} ]; then
 CONTEXT_DIR_PARAM="--context-dir=${GIT_CONTEXT_DIR}"
@@ -25,4 +26,4 @@ IMAGE_LIST=$(cat ${FROM_IMAGE_STREAM_FILE} | yq -r ".spec.tags[] | .from.name")
 LAST_IMAGE=$(echo ${IMAGE_LIST} | awk '{print $(NF)}')
 IMAGE_NAME=$(echo ${LAST_IMAGE} | awk -F'/' '{print $3}')
 
-oc new-app ${TO_NAMESPACE}/${IMAGE_NAME}~${GIT_URL} ${CONTEXT_DIR_PARAM} -n ${NAMESPACE}
+oc new-app ${LOCAL_IMAGE_STREAM_NAMESPACE}/${IMAGE_NAME}~${GIT_URL} ${CONTEXT_DIR_PARAM} -n ${NAMESPACE}
